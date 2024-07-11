e8a8b7efda1e64cdb2c7d9b3515b386937fa649e
sunainaralia <124876937+sunainaralia@users.noreply.github.com>
2024-07-11 12:35:56 +0530

    some mod

--- TiffinService/settings.py
+++ TiffinService/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-pfzbaoe(cpcj0szuva@wrs6&fh8$wm0!b9gy_&m+zrp8e*we#y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
